967e22603eae262bf9eff28aace2ccf48e3c3152
Sam Gespass and Zach Evans <zach@>
2017-11-28 15:20:08 -0800

    Add html, javascript, css, images.

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $(".turtle").click(function(){
+    $("#turtle-eggs").toggle();
+  });
+  $("#turtle-eggs").click(function(){
+    $("#turtle-eggs").toggle();
+  });
+  $("button#cat").click(function() {
+    $("ul#cat").prepend("<li>meow</li>")
+    $("ul#dog").prepend("<li>bark</li>")
+    $("ul#cat").children("li").first().click(function() {
+      alert("meow");
+    });
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      alert('bark');
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("button#dog").click(function() {
+    $("ul#dog").prepend("<li>bark</li>")
+    $("ul#cat").prepend("<li>meow</li>")
+    $("ul#dog").children("li").first().click(function() {
+      alert('bark');
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#cat").children("li").first().click(function() {
+      alert("meow");
+    });
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("button#poop").click(function() {
+    $("body").toggleClass("poop")
+  })
+});
